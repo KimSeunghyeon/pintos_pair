9a4e02a240a424b6a933ab14e0a51d687d3968f8
"Sungpil Woo" <"woosungpil7@gmail.com">
2014-03-28 02:32:29 +0900

    adfasdfasfasfsf

--- pintos/src/threads/thread.c
+++ pintos/src/threads/thread.c
@@ -18,6 +18,7 @@
 //sp
 /////ss
 ////ok
+/////3333
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
    of thread.h for details. */
