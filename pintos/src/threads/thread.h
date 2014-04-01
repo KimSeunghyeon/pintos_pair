13ee42cf38489c5b03b485b509ed5b1fe1abc2d6
"Sungpil Woo" <"woosungpil7@gmail.com">
2014-04-01 15:06:24 +0900

    asdfdasfasdfasfs

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -100,6 +100,7 @@ struct thread
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
+    struct file *loaded_file;
 #endif
 
     /* Owned by thread.c. */
